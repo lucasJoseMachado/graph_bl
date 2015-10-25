ef685b23a9fb738423d0bbe79ea98fc7c36be30b
Lucas Machado <lucas.machado@peopleone.com>
2015-10-25 17:03:30 -0200

    testando extensão

--- graphBL/app/models/path_planner.rb
+++ graphBL/app/models/path_planner.rb
@@ -1,5 +1,10 @@
 class PathPlanner
   def self.execute point_a, point_b
-    path = GraphDatabase.connection.get_extension("/example/bike_lane_proposal/from/#{point_a}/to/#{point_b}")
+    # path = GraphDatabase.connection.get_extension("/example/bike_lane_proposal/from/#{point_a}/to/#{point_b}")
+    request = Net::HTTP.get(URI.parse("http://localhost:7474/example/bike_lane_proposal/from/#{point_a}/to/#{point_b}"))
+    if request.present?
+      path = JSON.parse(request)
+    end
+    path
   end
 end
