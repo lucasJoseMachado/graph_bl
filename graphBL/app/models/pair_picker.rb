f8e2b0c6dcfd06c9ec0666295b1ffa7712f9ab0e
Lucas Machado <lucas.machado@peopleone.com>
2015-11-02 21:17:18 -0200

    corrigindo seleçao de pares

--- graphBL/app/models/pair_picker.rb
+++ graphBL/app/models/pair_picker.rb
@@ -5,20 +5,20 @@ class PairPicker
 
   private
     def self.pair_select_query
-      "MATCH (a)-[r:Bike]-(c:Point)
-        WHERE a.score <> 0
-          AND c.cluster_id = a.cluster_id
+      "MATCH (a:Point)
+        WHERE a.score IS NOT NULL
+          AND a.score > 0
       WITH DISTINCT a as a
-        MATCH (a)-[r:Car]-(c:Point)
+        MATCH (a)-[r]-(c:Point)
         WHERE c.cluster_id <> a.cluster_id OR c.cluster IS NULL
       WITH DISTINCT a as a
-        MATCH (b)-[r:Bike]-(c:Point)
+        MATCH (b:Point)
           WHERE id(a) < id(b)
-            AND b.score <> 0
+            AND b.score IS NOT NULL
+            and b.score > 0
             AND a.cluster_id < b.cluster_id
-            AND c.cluster_id = b.cluster_id
       WITH DISTINCT a as a, b as b
-        MATCH (b)-[r:Car]-(c:Point)
+        MATCH (b)-[r]-(c:Point)
         WHERE c.cluster_id <> b.cluster_id OR c.cluster IS NULL
       WITH DISTINCT a as a, b as b
         RETURN {origin: {id: a.id, score: a.score}, destination: {id: b.id, score: b.score}}
